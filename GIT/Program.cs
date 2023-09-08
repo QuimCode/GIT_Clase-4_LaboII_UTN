8df6f30da2fb864c1ead9df41164285f9653eacd
Quimey Alejo <quimafoncode@gmail.com>
2023-09-08 14:18:37 -0300

    Agregar archivos de proyecto.

---
+++ GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+/*  */
